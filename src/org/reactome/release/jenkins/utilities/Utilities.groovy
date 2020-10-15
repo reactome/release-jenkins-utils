e6f0b0a57eebf70c8591b6720329e2556305a346
Justin Cook <cookersjs@gmail.com>
2020-10-15 13:36:13 -0700

    Remove release version from arguments

--- src/org/reactome/release/jenkins/utilities/Utilities.groovy
+++ src/org/reactome/release/jenkins/utilities/Utilities.groovy
@@ -4,21 +4,28 @@ import groovy.json.JsonSlurper
 
 /**
  * Helper method for parsing out release number from Jenkins directory.
- * @return - Release number taken from directory (ex: 74)
+ * @return - Release number taken from directory (eg: 74)
  */
 def getReleaseVersion()
 {
     return (pwd() =~ /Releases\/(\d+)\//)[0][1];
 }
 
-def checkUpstreamBuildsSucceeded(String stepName, String currentRelease) {
-    def statusUrl = httpRequest authentication: 'jenkinsKey', validResponseCodes: "${env.VALID_RESPONSE_CODES}", url: "${env.JENKINS_JOB_URL}/job/$currentRelease/job/$stepName/lastBuild/api/json"
+/**
+ * Helper method for verifying that prerequisite steps have been run.
+ * It queries the Jenkins API and checks that the 'result' property is equal to 'SUCCESS'.
+ * An error will be thrown if the 'SUCCESS' message is not found.
+ * @param stepPath - Path in local Jenkins directory to requisite step (eg: Relational-Database-Updates/Orthoinference)
+ */
+def checkUpstreamBuildsSucceeded(String stepPath) {
+    def releaseVersion = getReleaseVersion()
+    def statusUrl = httpRequest authentication: 'jenkinsKey', validResponseCodes: "${env.VALID_RESPONSE_CODES}", url: "${env.JENKINS_JOB_URL}/job/$releaseVersion/job/$stepPath/lastBuild/api/json"
     if (statusUrl.getStatus() == 404) {
-        error("$stepName has not yet been run. Please complete a successful build.")
+        error("$stepPath has not yet been run. Please complete a successful build.")
     } else {
         def statusJson = new JsonSlurper().parseText(statusUrl.getContent())
         if(statusJson['result'] != "SUCCESS"){
-            error("Most recent $stepName build status: " + statusJson['result'] + ". Please complete a successful build.")
+            error("Most recent $stepPath build status: " + statusJson['result'] + ". Please complete a successful build.")
         }
     }
 }
