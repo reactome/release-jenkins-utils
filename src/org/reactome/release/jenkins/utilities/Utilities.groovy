7ee1719ac754a7ef9c4c8e47388939d943129eac
Justin Cook <cookersjs@gmail.com>
2020-10-15 13:56:55 -0700

    Revert to original usage

--- src/org/reactome/release/jenkins/utilities/Utilities.groovy
+++ src/org/reactome/release/jenkins/utilities/Utilities.groovy
@@ -30,11 +30,9 @@ def checkUpstreamBuildsSucceeded(String stepPath) {
 }
 
 def takeDatabaseDumpAndGzip(String database, String stepName, String beforeOrAfter, String username, String password, String host) {
-    sh "echo ${database}"
-    sh "echo $database"
-   // def timestamp = new Date().format("yyyy-MM-dd-HHmmss")
-    //def releaseVersion = getReleaseVersion()
-   // def filename = "$database_$releaseVersion_$beforeOrAfter_$stepName_$timestamp.dump"
-   // sh "mysqldump -u$user -p$pass -h$host $database > $filename"
-   // sh "gzip -f $filename"
+    def timestamp = new Date().format("yyyy-MM-dd-HHmmss")
+    def releaseVersion = getReleaseVersion()
+    def filename = "$database_$releaseVersion_$beforeOrAfter_$stepName_$timestamp.dump"
+    sh "mysqldump -u$user -p$pass -h$host $database > $filename"
+    sh "gzip -f $filename"
 }
