933d6c9b6bd244eac5c90fac395ecbf9643f56fb
Justin Cook <cookersjs@gmail.com>
2020-10-16 13:05:16 -0700

    Change type to ArrayList

--- src/org/reactome/release/jenkins/utilities/Utilities.groovy
+++ src/org/reactome/release/jenkins/utilities/Utilities.groovy
@@ -89,7 +89,7 @@ def buildJarFile() {
     sh "mvn clean compile assembly:single"
 }
 
-def cleanUpAndArchiveBuildFiles(String stepName, List dataFiles) {
+def cleanUpAndArchiveBuildFiles(String stepName, ArrayList dataFiles) {
     def releaseVersion = getReleaseVersion()
     def s3Path = "${env.S3_RELEASE_DIRECTORY_URL}/${releaseVersion}/${stepName}"
 
@@ -109,7 +109,7 @@ def cleanUpAndArchiveBuildFiles(String stepName, List dataFiles) {
 
 }
 
-def moveFilesToFolder(String folder, List files) {
+def moveFilesToFolder(String folder, ArrayList files) {
     for (String file : files) {
         sh "mv --backup=numbered ${file} ${folder}"
     }
