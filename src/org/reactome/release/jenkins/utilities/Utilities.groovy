3ae177887bc103dc0396d49fd586834f4f4f3460
Justin Cook <cookersjs@gmail.com>
2020-10-19 09:39:20 -0700

    Try declaring type

--- src/org/reactome/release/jenkins/utilities/Utilities.groovy
+++ src/org/reactome/release/jenkins/utilities/Utilities.groovy
@@ -128,13 +128,14 @@ def moveFilesToFolder(String folder, List files) {
 }
 
 def outputLineCountsOfFilesBetweenFolders(String firstFolderName, String secondFolderName) {
-    def firstDir = new File(firstFolderName)
-    def secondDir = new File(secondFolderName)
+    File firstDir = new File(Paths.get(firstFolderName))
+    File secondDir = new File(secondFolderName)
+    Paths.get(firstDir).
     println firstFolderName
     println firstDir
     println firstDir.listFiles()
     sh "ls -lrt ${firstDir}"
-    for (File file1 : firstDir.listFiles()) {
+    for (File file1 : firstDir.eac) {
         File file2 = secondFolderName + "/" + file1.getName()
         println file1
         println file2
